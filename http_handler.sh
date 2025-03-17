4f2cf28713ac1a217e0ef5844a9b45a842007eb6
anthraxisbr <gabrielmouraodemelo@gmail.com>
2025-03-16 21:26:15 -0300

    trying to implement a webserver using cobol

--- http_handler.sh
+++ http_handler.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
 
-# Function to generate HTML dynamically based on the URL
 generate_html() {
     url_path="$1"
     output_file="output.html"
     echo "<html><body><h1>Hello, $url_path!</h1></body></html>" > "$output_file"
 }
 
-# Read the incoming request line by line
 while read -r line; do
-    # Process only the first line of the request (the GET request)
     if [[ "$line" =~ GET\ /(.*)\ HTTP ]]; then
         url_path="${BASH_REMATCH[1]}"
         if [[ -z "$url_path" ]]; then
-            url_path="world"  # Default if no path is provided
+            url_path="world"
         fi
 
-        # Generate the HTML based on the URL path
         generate_html "$url_path"
 
-        # Send the HTTP response and close the connection
         {
             echo -e "HTTP/1.1 200 OK\r"
             echo -e "Content-Type: text/html\r"
-            echo -e "Connection: close\r"  # Close the connection after response
-            echo -e "\r"  # Empty line separating headers from body
+            echo -e "Connection: close\r"
+            echo -e "\r"
             cat "output.html"
         }
     fi
